3b7b59d5f9545d253ed765505477f19d7a2fa024
ihsanerben <ihsanerben@gmail.com>
2026-08-06 15:37:44 +0300

    feat: integrate Socket.IO notifications

---
+++ src/main/java/com/ihsanerben/ecommerce_simulation_api/websocket/SocketIOServerConfig.java
@@ -0,0 +1,38 @@
+package com.ihsanerben.ecommerce_simulation_api.websocket;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.ihsanerben.ecommerce_simulation_api.service.ApplicationConfigService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import static com.ihsanerben.ecommerce_simulation_api.config.ApplicationConfigKeys.SECURITY_ALLOWED_ORIGIN;
+
+@Configuration
+@RequiredArgsConstructor
+public class SocketIOServerConfig {
+
+    private final ApplicationConfigService applicationConfigService;
+
+    @Value("${app.socket-io.port:9092}")
+    private int port;
+
+    @Bean(destroyMethod = "stop")
+    SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration configuration =
+                new com.corundumstudio.socketio.Configuration();
+        configuration.setHostname("0.0.0.0");
+        configuration.setPort(port);
+        configuration.setOrigin(applicationConfigService.getValue(SECURITY_ALLOWED_ORIGIN));
+        return new SocketIOServer(configuration);
+    }
+
+    @Bean
+    @Profile("!test")
+    ApplicationRunner socketIOServerRunner(SocketIOServer socketIOServer) {
+        return arguments -> socketIOServer.start();
+    }
+}
